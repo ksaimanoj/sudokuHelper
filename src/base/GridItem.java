59d2c2677f703b31e8e66db748aa31a02b8ee11d
Sai Manoj Kadiyala <saimanoj.kadiyala@gmail.com>
2018-10-04 20:31:53 +0530

    Handling of possible values in GridItem

--- src/base/GridItem.java
+++ src/base/GridItem.java
@@ -2,7 +2,38 @@ package base;
 
 public class GridItem {
 	
-	int value;
-	boolean[] possibleValues;
+	public static final int MAXIMUM_POSSIBLE_VALUES = 9;
+	
+	private int value;
+	private boolean[] possibleValues;
+	
+	public GridItem() {
+		super();
+		possibleValues = new boolean[MAXIMUM_POSSIBLE_VALUES];
+	}
 
+	public void setValue(int value) {
+		this.value = value;
+		if(value != 0)
+		{
+			for(int i = 0; i < MAXIMUM_POSSIBLE_VALUES; i++)
+				possibleValues[i] = false;
+		}
+	}
+	
+	public int getValue()
+	{
+		return this.value;
+	}
+	
+	public void setPossibleValueFalse(int value)
+	{
+		possibleValues[value] = false;
+	}
+	
+	public boolean isValueFixed()
+	{
+		return this.value != 0;
+	}
+	
 }
