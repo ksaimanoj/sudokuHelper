5f2369a1e7c4ea2d7807d9ff71936fd2bfe0468b
Sai Manoj Kadiyala <saimanoj.kadiyala@gmail.com>
2018-10-04 20:31:09 +0530

    Added basic logic to set possible values within the row and column false.

--- src/base/Board.java
+++ src/base/Board.java
@@ -10,6 +10,38 @@ public class Board {
 	public Board(int[] gridNumbers) {
 		items = new GridItem[LENGTH][WIDTH];
 		initializeBoard();
+		initializeValues(gridNumbers);
+	}
+	
+	public void solve()
+	{
+		for(int row = 0; row < LENGTH; row++)
+		{
+			for(int col = 0; col < WIDTH; col++)
+			{
+				if(items[row][col].isValueFixed())
+					solveIndividualItem(row, col);
+			}
+		}
+	}
+
+	private void solveIndividualItem(int row, int col) {
+		handleAllRowItems(row, col);
+		handleAllColumnItems(row, col);
+	}
+
+	private void handleAllRowItems(int row, int col) {
+		for(int colNo = 0; colNo < GridItem.MAXIMUM_POSSIBLE_VALUES; colNo++)
+		{
+			items[row][colNo].setPossibleValueFalse(items[row][col].getValue());
+		}
+	}
+
+	private void handleAllColumnItems(int row, int col) {
+		for(int rowNo = 0; rowNo < GridItem.MAXIMUM_POSSIBLE_VALUES; rowNo++)
+		{
+			items[rowNo][col].setPossibleValueFalse(items[row][col].getValue());
+		}
 	}
 
 	private void initializeBoard() {
@@ -17,9 +49,23 @@ public class Board {
 		{
 			for(int col = 0; col < WIDTH; col++)
 			{
-				items[row][col] = new GridItem();
+				solveIndividualItem(row, col);
 			}
 		}
+	}
+
+	private void initializeValues(int[] gridNumbers) {
+		for(int row = 0; row < LENGTH; row++)
+		{
+			for(int col = 0; col < WIDTH; col++)
+			{
+				items[row][col].setValue(gridNumbers[obtainSequenceNumber(row, col)]);
+			}
+		}
+	}
+
+	private int obtainSequenceNumber(int row, int col) {
+		return row * GridItem.MAXIMUM_POSSIBLE_VALUES + col;
 	}
 	
 	public int getNumbers()
