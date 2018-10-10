91492f1df78fbd87626ab0a6b4cddfb69a638f69
Sai Manoj Kadiyala <saimanoj.kadiyala@gmail.com>
2018-10-10 08:57:43 +0530

    Added new 2 star sudoku

--- src/base/GridItem.java
+++ src/base/GridItem.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GridItem {
-	
+
 	public static final int MAXIMUM_POSSIBLE_VALUE = 9;
-	
+
 	private int value;
 	private boolean[] possibleValues;
-	
+
 	public GridItem() {
 		super();
 		initializeAllPossibleValuesAsTrue();
@@ -23,45 +23,38 @@ public class GridItem {
 	public void setValue(int value) {
 		this.value = value;
 		initializeAllPossibleValuesAsTrue();
-		if(isValueKnown())
+		if (isValueKnown())
 			setAllPossibleValues(false);
 	}
 
 	private void setAllPossibleValues(boolean state) {
-		for(int i = 0; i < MAXIMUM_POSSIBLE_VALUE; i++)
+		for (int i = 0; i < MAXIMUM_POSSIBLE_VALUE; i++)
 			possibleValues[i] = state;
 	}
-	
-	public int value()
-	{
+
+	public int value() {
 		return this.value;
 	}
-	
-	public void changePossibleValue(int index, boolean state)
-	{
+
+	public void changePossibleValue(int index, boolean state) {
 		possibleValues[index - 1] = state;
 	}
-	
-	public boolean isPossible(int index)
-	{
+
+	public boolean isPossible(int index) {
 		return possibleValues[index];
 	}
-	
-	public boolean isValueKnown()
-	{
+
+	public boolean isValueKnown() {
 		return this.value != 0;
 	}
 
 	public List<Integer> getPossibleValues() {
 		List<Integer> possibleIntegerList = new ArrayList<>();
-		for(int i = 0; i < GridItem.MAXIMUM_POSSIBLE_VALUE; i++)
-		{
-			if(possibleValues[i])
+		for (int i = 0; i < GridItem.MAXIMUM_POSSIBLE_VALUE; i++) {
+			if (possibleValues[i])
 				possibleIntegerList.add(i + 1);
 		}
 		return possibleIntegerList;
 	}
-	
-	
-	
+
 }
--- src/base/GridItemTest.java
+++ src/base/GridItemTest.java
@@ -9,115 +9,103 @@ import java.util.List;
 import org.junit.*;
 
 public class GridItemTest {
-	
+
 	GridItem gridItem;
-	
+
 	@Before
-	public void setUp()
-	{
+	public void setUp() {
 		gridItem = new GridItem();
 	}
-	
+
 	private void allPossibleValueMustBeFalse() {
-		for(int index = 0; index < GridItem.MAXIMUM_POSSIBLE_VALUE; index++)
-		{
+		for (int index = 0; index < GridItem.MAXIMUM_POSSIBLE_VALUE; index++) {
 			assertFalse(gridItem.isPossible(index));
 		}
 	}
-		
+
 	private void allPossibleValuesMustBeTrue() {
-		for(int index = 0; index < GridItem.MAXIMUM_POSSIBLE_VALUE; index++)
-		{
+		for (int index = 0; index < GridItem.MAXIMUM_POSSIBLE_VALUE; index++) {
 			assertTrue(gridItem.isPossible(index));
 		}
 	}
-	
+
 	private boolean compareLists(List<Integer> list1, List<Integer> list2) {
-		if(list1.size() != list2.size())
+		if (list1.size() != list2.size())
 			return false;
-		for(int i = 0; i < list1.size(); i++)
-		{
-			if(list1.get(i) != list2.get(i))
+		for (int i = 0; i < list1.size(); i++) {
+			if (list1.get(i) != list2.get(i))
 				return false;
 		}
 		return true;
 	}
-	
+
 	private List<Integer> getFullList(int startIndex) {
 		List<Integer> fullList = new ArrayList<>();
-		for(int i = 0; i < GridItem.MAXIMUM_POSSIBLE_VALUE; i++)
+		for (int i = 0; i < GridItem.MAXIMUM_POSSIBLE_VALUE; i++)
 			fullList.add(i + startIndex);
 		return fullList;
 	}
-	
+
 	@Test
-	public void initializePossibleValuesTest()
-	{
-		for(int index = 0; index < GridItem.MAXIMUM_POSSIBLE_VALUE; index++)
+	public void initializePossibleValuesTest() {
+		for (int index = 0; index < GridItem.MAXIMUM_POSSIBLE_VALUE; index++)
 			assertTrue(gridItem.isPossible(index));
 	}
-	
+
 	@Test
-	public void setValueNonZeroTest()
-	{
+	public void setValueNonZeroTest() {
 		gridItem.setValue(3);
 		allPossibleValueMustBeFalse();
 	}
 
-	
 	@Test
-	public void setValueZeroTest()
-	{
+	public void setValueZeroTest() {
 		gridItem.setValue(0);
 		allPossibleValuesMustBeTrue();
 	}
+
 	@Test
-	public void setPossibleValueFalseTest()
-	{
+	public void setPossibleValueFalseTest() {
 		gridItem.changePossibleValue(4, false);
-		
-		for(int index = 0; index < 4 - 1; index++)
-		{
+
+		for (int index = 0; index < 4 - 1; index++) {
 			assertTrue(gridItem.isPossible(index));
 		}
-		
+
 		assertFalse(gridItem.isPossible(4 - 1));
-		
-		for(int index = 4; index < GridItem.MAXIMUM_POSSIBLE_VALUE; index++)
-		{
+
+		for (int index = 4; index < GridItem.MAXIMUM_POSSIBLE_VALUE; index++) {
 			assertTrue(gridItem.isPossible(index));
 		}
 	}
-	
+
 	@Test
-	public void isValueFixedTest()
-	{
+	public void isValueFixedTest() {
 		gridItem.setValue(3);
 		assertTrue(gridItem.isValueKnown());
-		
+
 		gridItem.setValue(0);
 		assertFalse(gridItem.isValueKnown());
 	}
-	
+
 	@Test
-	public void compareTwoArraysTest()
-	{
+	public void compareTwoArraysTest() {
 		List<Integer> emptyList1 = new ArrayList<>();
 		List<Integer> emptyList2 = new ArrayList<>();
 		Assert.assertTrue(compareLists(emptyList1, emptyList2));
-		
+
 		List<Integer> nonEmptyList1 = new ArrayList<>();
 		nonEmptyList1.add(1);
 		Assert.assertFalse(compareLists(emptyList1, nonEmptyList1));
-		
+
 		List<Integer> nonEmptyList2 = new ArrayList<>();
 		nonEmptyList2.add(1);
 		Assert.assertTrue(compareLists(nonEmptyList1, nonEmptyList2));
-		
+
 		List<Integer> nonEmptyList3 = new ArrayList<>();
 		nonEmptyList3.add(2);
 		Assert.assertFalse(compareLists(nonEmptyList1, nonEmptyList3));
-		
+
 		List<Integer> nonEmptyList4 = new ArrayList<>();
 		nonEmptyList4.add(1);
 		nonEmptyList4.add(2);
@@ -133,18 +121,18 @@ public class GridItemTest {
 		nonEmptyList5.add(7);
 		nonEmptyList5.add(9);
 		Assert.assertFalse(compareLists(nonEmptyList4, nonEmptyList5));
-		
+
 		List<Integer> fullList1 = getFullList(0);
 		List<Integer> fullList2 = getFullList(0);
 		Assert.assertEquals(fullList1, fullList2);
 	}
 
 	@Test
-	public void getPossibleValuesTest()
-	{
+	public void getPossibleValuesTest() {
 		List<Integer> fullList1 = getFullList(1);
-		Assert.assertTrue(compareLists(fullList1, gridItem.getPossibleValues()));
-		
+		Assert.assertTrue(
+				compareLists(fullList1, gridItem.getPossibleValues()));
+
 		fullList1 = new ArrayList<>();
 		fullList1.add(1);
 		fullList1.add(2);
@@ -155,7 +143,8 @@ public class GridItemTest {
 		fullList1.add(9);
 		gridItem.changePossibleValue(5, false);
 		gridItem.changePossibleValue(6, false);
-		Assert.assertTrue(compareLists(fullList1, gridItem.getPossibleValues()));
+		Assert.assertTrue(
+				compareLists(fullList1, gridItem.getPossibleValues()));
 	}
 
 }
