1581b6d9ba43ecee119138eab3c0e55d5743cfe0
Sai Manoj Kadiyala <saimanoj.kadiyala@gmail.com>
2018-10-11 07:59:33 +0530

    The solver is able to find 8 values for a 1 star sudoku. Need to find a better way of doing this. For 2 star and above sudoku this algorithm is of no use.

--- src/solver/SimpleSolver.java
+++ src/solver/SimpleSolver.java
@@ -12,33 +12,44 @@ public class SimpleSolver implements Solver {
 	public Board solve(Board board) {
 		Board newBoard = new Board();
 
-		GridItem[][] items = board.getState();
-		for (int row = 0; row < Board.LENGTH; row++) {
-			for (int col = 0; col < Board.WIDTH; col++) {
-				if (items[row][col].isValueKnown()) {
-					int gridNo = getGridNumber(row, col);
-					board.removeValueFromRow(row, items[row][col].value());
-					board.removeValueFromColumn(col, items[row][col].value());
-					board.removeValueFromBoard(gridNo, items[row][col].value());
-				}
-			}
-		}
-
-		for (int row = 0; row < Board.LENGTH; row++) {
-			for (int col = 0; col < Board.WIDTH; col++) {
-				if (!items[row][col].isValueKnown()) {
-					List<Integer> possibleValues = items[row][col]
-							.getPossibleValues();
-					//System.out.println("possibleValues: " + possibleValues);
-					if(possibleValues.size() == 1)
-					{
-						System.out.println("row: " + row);
-						System.out.println("col: " + col);
-						System.out.println("possibleValues: " + possibleValues);
+		int count = 0;
+		do {
+			GridItem[][] items = board.getState();
+			for (int row = 0; row < Board.LENGTH; row++) {
+				for (int col = 0; col < Board.WIDTH; col++) {
+					if (items[row][col].isValueKnown()) {
+						int gridNo = getGridNumber(row, col);
+						board.removeValueFromRow(row, items[row][col].value());
+						board.removeValueFromColumn(col,
+								items[row][col].value());
+						board.removeValueFromBoard(gridNo,
+								items[row][col].value());
 					}
 				}
 			}
-		}
+
+			count = 0;
+			
+			for (int row = 0; row < Board.LENGTH; row++) {
+				for (int col = 0; col < Board.WIDTH; col++) {
+					if (!items[row][col].isValueKnown()) {
+						List<Integer> possibleValues = items[row][col]
+								.getPossibleValues();
+						// System.out.println("possibleValues: " +
+						// possibleValues);
+						if (possibleValues.size() == 1) {
+							count++;
+							items[row][col].setValue(possibleValues.get(0));
+							System.out.print("row: " + row);
+							System.out.print(" col: " + col);
+							System.out.println(
+									" possibleValues: " + possibleValues);
+						}
+					}
+				}
+			}
+			System.out.println("count: " + count);
+		} while(count > 0);
 
 		newBoard.setState(board.getState());
 		return newBoard;
